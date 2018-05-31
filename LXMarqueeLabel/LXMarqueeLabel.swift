82fb2bfb2960e9a703c2291e8d480e24b0886fa2
从今以后 <949478479@qq.com>
2018-05-31 10:35:11 +0800

    增加边缘渐变遮罩效果并完善一些细节

--- LXMarqueeLabel/LXMarqueeLabel.swift
+++ LXMarqueeLabel/LXMarqueeLabel.swift
@@ -20,8 +20,8 @@ class LXMarqueeLabel: UIView {
 
     private(set) var state: State = .stopped
 
-    /// 文本间距，只能在 stopped 状态下设置
-    @IBInspectable var textSpacing: CGFloat = 50 {
+    /// 文本间距，默认 40，只能在 stopped 状态下设置
+    @IBInspectable var textSpacing: CGFloat = 40 {
         willSet {
             precondition(state == .stopped)
         }
@@ -36,45 +36,49 @@ class LXMarqueeLabel: UIView {
 
     /// 文本颜色，默认 black
     @IBInspectable var textColor: UIColor = .black {
-        willSet {
-            offscreenTextLabels.forEach { $0.textColor = newValue }
+        didSet {
+            adjustTextLabelColor()
         }
     }
 
     /// 文本字体，默认 15 system font，只能在 stopped 状态下设置
-    var font: UIFont = .systemFont(ofSize: 15) {
-        willSet {
+    var textFont = UIFont.systemFont(ofSize: 15) {
+        didSet {
             precondition(state == .stopped)
-            offscreenTextLabels.forEach { $0.font = newValue }
+            adjustTextLabelFont()
         }
     }
 
     /// 文本列表
-    var textList: [String] = [] {
-        willSet {
-            stop()
-            nextIndex = newValue.isEmpty ? -1 : 0
+    var textList = [String]() {
+        willSet { stop() }
+        didSet { resetIndex() }
+    }
+
+    /// 渐变遮罩的宽度，默认为 20
+    @IBInspectable var fadeWidth: CGFloat = 20 {
+        didSet {
+            layoutGradientMasks()
+            setGradientMaskHidden(!(isGradientMaskColorVisible() && fadeWidth > 0))
         }
     }
 
     override var backgroundColor: UIColor? {
         didSet {
-            offscreenTextLabels.forEach { $0.backgroundColor = backgroundColor }
+            adjustGradientMaskColor()
+            adjustContentBackgroundColor()
         }
     }
 
-    private var nextIndex: Int = -1 {
-        didSet {
-            if nextIndex > textList.count - 1 {
-                nextIndex = 0
-            }
-        }
-    }
-
-    private var onscreenTextLabels: [TextLabel] = []
-    private var offscreenTextLabels: [TextLabel] = []
-
+    private var nextIndex = NSNotFound
     private var displayLink: CADisplayLink?
+
+    private let textLabelContainerView = UIView()
+    private var onscreenTextLabels = [TextLabel]()
+    private var offscreenTextLabels = [TextLabel]()
+
+    private let leftGradientMask = CAGradientLayer()
+    private let rightGradientMask = CAGradientLayer()
 
     deinit {
         invalidateDisplayLink()
@@ -82,22 +86,13 @@ class LXMarqueeLabel: UIView {
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        clipsToBounds = true
+        commonInit()
     }
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
-        clipsToBounds = true
+        commonInit()
     }
-
-    override func layoutSubviews() {
-        super.layoutSubviews()
-        onscreenTextLabels.forEach { $0.center.y = frame.height * 0.5 }
-    }
-}
-
-private extension LXMarqueeLabel {
-    class TextLabel: UILabel {}
 }
 
 // MARK: - 滚动控制
@@ -109,7 +104,7 @@ extension LXMarqueeLabel {
         guard !textList.isEmpty else { return }
 
         if state == .stopped {
-            addNextTextLabel()
+            addOnscreenTextLabel()
         }
 
         if window != nil {
@@ -129,40 +124,194 @@ extension LXMarqueeLabel {
     /// 停止滚动
     func stop() {
         guard !isStopped else { return }
-        nextIndex = 0
+        resetIndex()
         pauseDisplayLink()
-        onscreenTextLabels.forEach { $0.removeFromSuperview() }
-        onscreenTextLabels.removeAll(keepingCapacity: true)
+        clearOnscreenTextLabels()
+        resetContainerViewBounds()
         state = .stopped
     }
 }
 
-// MARK: - 循环利用
+// MARK: - LXMarqueeLabel.TextLabel
+private extension LXMarqueeLabel {
+    class TextLabel: UILabel {}
+}
+
+// MARK: - 初始化
+private extension LXMarqueeLabel {
+
+    func commonInit() {
+        textLabelContainerView.clipsToBounds = true
+        addSubview(textLabelContainerView)
+
+        leftGradientMask.startPoint = CGPoint(x: 0, y: 0.5)
+        leftGradientMask.endPoint = CGPoint(x: 1, y: 0.5)
+        layer.addSublayer(leftGradientMask)
+
+        rightGradientMask.startPoint = CGPoint(x: 0, y: 0.5)
+        rightGradientMask.endPoint = CGPoint(x: 1, y: 0.5)
+        layer.addSublayer(rightGradientMask)
+
+        adjustGradientMaskColor()
+    }
+}
+
+// MARK: - 布局
 extension LXMarqueeLabel {
+
+    override var bounds: CGRect {
+        didSet {
+            guard bounds != oldValue else { return }
+            layoutSubviews2()
+        }
+    }
+
+    override var frame: CGRect {
+        didSet {
+            guard frame != oldValue else { return }
+            layoutSubviews2()
+        }
+    }
+
+    private func layoutSubviews2() {
+        layoutContainerView()
+        layoutGradientMasks()
+        layoutOnscreenTextLabels()
+    }
+
+    private func layoutContainerView() {
+        textLabelContainerView.frame = bounds
+    }
+
+    private func layoutGradientMasks() {
+        leftGradientMask.frame = CGRect(x: 0, y: 0, width: fadeWidth, height: bounds.height)
+        rightGradientMask.frame = CGRect(x: bounds.maxX - fadeWidth, y: 0, width: fadeWidth, height: bounds.height)
+    }
+
+    private func layoutOnscreenTextLabels() {
+        onscreenTextLabels.forEach { $0.center.y = bounds.midY }
+    }
+
+    private func resetContainerViewBounds() {
+        textLabelContainerView.bounds.origin = .zero
+    }
+}
+
+// MARK: - 字体
+private extension LXMarqueeLabel {
+
+    func adjustTextLabelFont() {
+        onscreenTextLabels.forEach { $0.font = textFont }
+        offscreenTextLabels.forEach { $0.font = textFont }
+    }
+}
+
+// MARK: - 颜色
+private extension LXMarqueeLabel {
+
+    func adjustGradientMaskColor() {
+        guard let startColor = backgroundColor?.cgColor, startColor.alpha > 0 else {
+            setGradientMaskHidden(true)
+            return
+        }
+
+        let endColor = startColor.copy(alpha: 0)!
+
+        leftGradientMask.isHidden = fadeWidth <= 0
+        leftGradientMask.colors = [startColor, endColor]
+
+        rightGradientMask.isHidden = fadeWidth <= 0
+        rightGradientMask.colors = [endColor, startColor]
+    }
+
+    func adjustTextLabelColor() {
+        onscreenTextLabels.forEach { $0.textColor = textColor }
+        offscreenTextLabels.forEach { $0.textColor = textColor }
+    }
+
+    func adjustContentBackgroundColor() {
+        textLabelContainerView.backgroundColor = backgroundColor
+
+        let clipsToBounds: Bool = {
+            if let color = backgroundColor?.cgColor, color.alpha > 0 {
+                return true
+            }
+            return false
+        }()
+
+        onscreenTextLabels.forEach {
+            $0.clipsToBounds = clipsToBounds
+            $0.backgroundColor = backgroundColor
+        }
+
+        offscreenTextLabels.forEach {
+            $0.clipsToBounds = clipsToBounds
+            $0.backgroundColor = backgroundColor
+        }
+    }
+
+    func isGradientMaskColorVisible() -> Bool {
+        if let color = backgroundColor?.cgColor, color.alpha > 0 {
+            return true
+        }
+        return false
+    }
+}
+
+// MARK: - 渐变遮罩
+private extension LXMarqueeLabel {
+
+    func setGradientMaskHidden(_ hidden: Bool) {
+        leftGradientMask.isHidden = hidden
+        rightGradientMask.isHidden = hidden
+    }
+}
+
+// MARK: - 更新索引
+private extension LXMarqueeLabel {
+
+    func increaseIndex() {
+        nextIndex = (nextIndex + 1) % textList.count
+    }
+
+    func resetIndex() {
+        nextIndex = textList.isEmpty ? NSNotFound : 0
+    }
+}
+
+// MARK: - 循环利用
+private extension LXMarqueeLabel {
     
-    private func dequeueReusableTextLabel() -> TextLabel {
+    func dequeueReusableTextLabel() -> TextLabel {
         if let textLabel = offscreenTextLabels.popLast() {
             return textLabel
         }
+
         let textLabel = TextLabel()
-        textLabel.font = font
-        textLabel.clipsToBounds = true
+        textLabel.font = textFont
         textLabel.textColor = textColor
         textLabel.backgroundColor = backgroundColor
+
+        if let color = backgroundColor?.cgColor, color.alpha > 0 {
+            textLabel.clipsToBounds = true
+        } else {
+            textLabel.clipsToBounds = false
+        }
+
         return textLabel
     }
 
-    private func recycle(_ textLabel: TextLabel) {
+    func recycle(_ textLabel: TextLabel) {
         offscreenTextLabels.append(textLabel)
     }
 }
 
-// MARK: - 添加标签
+// MARK: - 添加移除标签
 private extension LXMarqueeLabel {
 
-    func addNextTextLabel() {
+    func addOnscreenTextLabel() {
         let currentIndex = nextIndex
-        nextIndex += 1
+        increaseIndex()
 
         let textLabel = dequeueReusableTextLabel()
         textLabel.text = textList[currentIndex]
@@ -171,11 +320,25 @@ private extension LXMarqueeLabel {
         if let lastLabelFrame = onscreenTextLabels.last?.frame {
             textLabel.frame.origin.x = lastLabelFrame.maxX + textSpacing
         } else {
-            textLabel.frame.origin.x = bounds.maxX
+            textLabel.frame.origin.x = textLabelContainerView.bounds.maxX
         }
 
-        addSubview(textLabel)
         onscreenTextLabels.append(textLabel)
+        textLabelContainerView.addSubview(textLabel)
+    }
+
+    func clearOnscreenTextLabels() {
+        onscreenTextLabels.forEach {
+            $0.removeFromSuperview()
+            recycle($0)
+        }
+        onscreenTextLabels.removeAll(keepingCapacity: true)
+    }
+
+    func removeOffscreenTextLabel() {
+        let textLabel = onscreenTextLabels.removeFirst()
+        textLabel.removeFromSuperview()
+        recycle(textLabel)
     }
 }
 
@@ -202,6 +365,7 @@ private extension LXMarqueeLabel {
     }
 
     class DisplayLinkTarget {
+
         weak var owner: LXMarqueeLabel?
 
         init(owner: LXMarqueeLabel) {
@@ -210,12 +374,20 @@ private extension LXMarqueeLabel {
 
         @objc func step(_ displayLink: CADisplayLink) {
             guard let marqueeLabel = owner else { return }
-            marqueeLabel.bounds.origin.x += marqueeLabel.textScrollSpeed * CGFloat(displayLink.duration)
-            if let firstLabelMaxX = marqueeLabel.onscreenTextLabels.first?.frame.maxX, firstLabelMaxX <= marqueeLabel.bounds.minX {
-                marqueeLabel.onscreenTextLabels.removeFirst().removeFromSuperview()
+
+            let originXOffset = marqueeLabel.textScrollSpeed * CGFloat(displayLink.duration)
+            marqueeLabel.textLabelContainerView.bounds.origin.x += originXOffset
+
+            if let firstLabel = marqueeLabel.onscreenTextLabels.first,
+                firstLabel.frame.maxX <= marqueeLabel.textLabelContainerView.bounds.minX
+            {
+                marqueeLabel.removeOffscreenTextLabel()
             }
-            if let lastLabelMaxX = marqueeLabel.onscreenTextLabels.last?.frame.maxX, marqueeLabel.bounds.maxX - lastLabelMaxX >= marqueeLabel.textSpacing {
-                marqueeLabel.addNextTextLabel()
+
+            if let lastLabelMaxX = marqueeLabel.onscreenTextLabels.last?.frame.maxX,
+                marqueeLabel.textLabelContainerView.bounds.maxX - lastLabelMaxX >= marqueeLabel.textSpacing
+            {
+                marqueeLabel.addOnscreenTextLabel()
             }
         }
     }
@@ -223,12 +395,6 @@ private extension LXMarqueeLabel {
 
 // MARK: - 视图关系变更
 extension LXMarqueeLabel {
-
-    override func willRemoveSubview(_ subview: UIView) {
-        if let textLabel = subview as? TextLabel {
-            recycle(textLabel)
-        }
-    }
 
 	override func willMove(toWindow newWindow: UIWindow?) {
 		if newWindow == nil {
